79770a180c6d8ae377e9632adabc189f65bae859
fariasleo1983 <leonardo.fariasrj@outlook.com>
2022-06-18 21:29:27 -0300

    primeiros testes com jacaScript

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var nome = "Leonardo Farias";
+var n1 = 5;
+var n2 = 3;
+var frase = "Jesus é o Senhor das nações!"
+
+//alert(nome + " tem " + idade + "anos!");
+//alert(n1 * n2);
+
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toUpperCase());
