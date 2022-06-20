4131a6c20cfa8e32fb758bec1f4d29c803a37ba3
fariasleo1983 <leonardo.fariasrj@outlook.com>
2022-06-20 19:28:23 -0300

    testes com jacaScript

--- js/main.js
+++ js/main.js
@@ -1,11 +1,68 @@
-var nome = "Leonardo Farias";
-var n1 = 5;
-var n2 = 3;
-var frase = "Jesus é o Senhor das nações!"
+var d = new Date();
+alert(d.getHours());
+
+
+/*var count;
+for (count = 0; count <= 5; count++) {
+    alert(count);
+};*/
+
+
+
+/*var count = 0;
+while (count <= 5) {
+    console.log(count);
+    alert(count);
+    count++;
+};*/
+
+
+/*var idade = prompt("Qual sua idade?");
+
+if (idade >= 18) {
+    alert("Maior de idade!");
+} else {
+    alert("Menor de idade!");
+};*/
+
+/*var frutas = [{ nome: "maça", cor: "vermelha" },
+    { nome: "uva", cor: "roxa" }
+]
+
+console.log(frutas);
+alert(frutas[1].nome);*/
+
+
+/*var fruta = { nome: "maça", cor: "vermelha" }
+
+console.log(fruta.cor);
+alert(fruta.cor);*/
+
+
+
+//var lista = ["Maça", "Pêra", "Laranja"];
+//lista.push("Uva");
+//lista.pop();
+
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" | "));
+
+
+//alert(lista[1]);
+
+
+
+
+//var nome = "Leonardo Farias";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Jesus é o Senhor das nações!"
 
 //alert(nome + " tem " + idade + "anos!");
 //alert(n1 * n2);
 
-console.log(nome);
-console.log(n1 * n2);
-console.log(frase.toUpperCase());
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.toUpperCase());
